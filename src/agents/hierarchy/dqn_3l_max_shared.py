1. change ee config on panel for the gripper offset. 2. fix swapped rx and ry. 3. remove the *0.8 normalization for depth image

src/agents/hierarchy/dqn_3l_max_shared.py
@@ -407,13 +407,13 @@ class DQN3LMaxShared:
     def loadModel(self, path_pre):
         q1_path = path_pre + '_q1.pt'
         print('loading {}'.format(q1_path))
-        self.q1.load_state_dict(torch.load(q1_path))
+        self.q1.load_state_dict(torch.load(q1_path, map_location='cuda:0'))
         q2_path = path_pre + '_q2.pt'
         print('loading {}'.format(q2_path))
-        self.q2.load_state_dict(torch.load(q2_path))
+        self.q2.load_state_dict(torch.load(q2_path, map_location='cuda:0'))
         q3_path = path_pre + '_q3.pt'
         print('loading {}'.format(q3_path))
-        self.q3.load_state_dict(torch.load(q3_path))
+        self.q3.load_state_dict(torch.load(q3_path, map_location='cuda:0'))
         self.updateTarget()
 
     def saveModel(self, path):

src/env.py
@@ -98,8 +98,8 @@ class Env:
             rx = 0
         elif self.action_sequence.count('r') == 2:
             rz = action[rot_idx]
-            ry = 0
-            rx = action[rot_idx + 1]
+            ry = action[rot_idx + 1]
+            rx = 0
         elif self.action_sequence.count('r') == 3:
             rz = action[rot_idx]
             ry = action[rot_idx + 1]
@@ -123,7 +123,7 @@ class Env:
         a = np.linspace(0.5, 1, 90).reshape(1, 90).repeat(90, axis=0).T
         b = b * a * 0.01
         obs -= b
-        obs *= 0.8
+        # obs *= 0.8
         obs[obs < 0.007] = 0
         return obs
 

src/main.py
@@ -357,7 +357,7 @@ if __name__ == '__main__':
     rospy.init_node('image_proxy')
 
     global model, alg, action_sequence, in_hand_mode, workspace, max_z, min_z
-    ws_center = [-0.5257, -0.0098, 0.09]
+    ws_center = [-0.5257, -0.0098, -0.08]
     workspace = np.asarray([[ws_center[0]-0.15, ws_center[0]+0.15],
                             [ws_center[1]-0.15, ws_center[1]+0.15],
                             [ws_center[2], 0.50]])
@@ -374,7 +374,10 @@ if __name__ == '__main__':
     # pre = '/home/dian/Downloads/dqfd/snapshot_house_building_1'
     # pre = '/home/dian/Downloads/dqfd/snapshot_block_stacking'
 
-    pre = '/home/dian/Downloads/4h1_7_sdqfd_perlin/ran_tilt_4h1_7_sdqfd_3l_qt_0628_4_12420761_2/models/snapshot_tilt_house_building_1'
+    # pre = '/home/dian/Downloads/4h1_7_sdqfd_perlin/ran_tilt_4h1_7_sdqfd_3l_qt_0628_4_12420761_2/models/snapshot_tilt_house_building_1'
+    # pre = '/home/dian/Downloads/4h1_9_sdqfd_perlin/models/snapshot_tilt_house_building_1'
+    pre = '/home/dian/Downloads/h3_9_sdqfd_perlin/models/snapshot_tilt_house_building_3'
+
     agent.loadModel(pre)
     agent.eval()
     # agent.initHis(1)
@@ -390,20 +393,25 @@ if __name__ == '__main__':
         state = torch.tensor([env.ur5.holding_state], dtype=torch.float32)
         q_map, action_idx, action = agent.getEGreedyActions(state, in_hand, obs, 0, 0)
         plt.imshow(obs[0, 0])
+        plt.colorbar()
         plt.axis('off')
-        plt.scatter(action_idx[0, 1], action_idx[0, 0])
+        plt.scatter(action_idx[0, 1], action_idx[0, 0], c='r')
         # plt.savefig(os.path.join('/home/dian/Documents/obs', '{}_obs.png'.format(j)))
         plt.show()
 
         # plotQMaps(q_map, '/home/dian/Documents/qmap', j)
         # plotQMaps(q_map)
         plt.imshow(q_map[0])
-        plt.scatter(action_idx[0, 1], action_idx[0, 0])
+        plt.scatter(action_idx[0, 1], action_idx[0, 0], c='r')
         plt.show()
         # pixels = action_idx[:, :2]
         # patch = agent.getImgPatch(obs, pixels)
         # action = torch.cat(action[0])
-        action = (*list(map(lambda x: x.item(), action[0])), state.item())
+        action = [*list(map(lambda x: x.item(), action[0])), state.item()]
+        z_offset_threshold = -0.015 if state.item() == 0 else 0.02
+        if action[2] - ws_center[2] < obs[0, 0, action_idx[0, 0], action_idx[0, 1]] + z_offset_threshold:
+            print('z {} too small, clipping to {}'.format(action[2], obs[0, 0, action_idx[0, 0], action_idx[0, 1]] + z_offset_threshold + ws_center[2]))
+            action[2] = (obs[0, 0, action_idx[0, 0], action_idx[0, 1]] + z_offset_threshold + ws_center[2]).item()
         env.step(action)
         
         # agent.updateHis(patch, action[:, 2], torch.tensor([env.ur5.holding_state], dtype=torch.float32), None, torch.zeros(1))

src/ur5.py
@@ -70,16 +70,16 @@ class UR5:
     def moveToP(self, x, y, z, rx, ry, rz):
         # rz = np.pi/2 + rz
 
-        z -= 0.15
-        T = transformation.euler_matrix(rx, ry, rz)
-        pos = np.array([x, y, z])
-        pos += 0.15 * T[:3, 2]
-        x, y, z = pos
+        # z -= 0.15
+        # T = transformation.euler_matrix(rx, ry, rz)
+        # pos = np.array([x, y, z])
+        # pos += 0.15 * T[:3, 2]
+        # x, y, z = pos
 
         rx, ry, rz = rpyToRotVector(rx, ry, rz)
         pose = [x, y, z, rx, ry, rz]
         for _ in range(1):
-            s = 'movel(p{},v=0.1)'.format(pose)
+            s = 'movel(p{})'.format(pose)
             rospy.sleep(0.5)
             self.pub.publish(s)
             self.waitUntilNotMoving()
@@ -93,7 +93,7 @@ class UR5:
         if self.holding_state:
             return
         rx, ry, rz = r
-        rz = np.pi/2 + rz
+        # rz = np.pi/2 + rz
         T = transformation.euler_matrix(rx, ry, rz)
         pre_pos = np.array([x, y, z])
         pre_pos += self.pick_offset * T[:3, 2]
@@ -113,7 +113,7 @@ class UR5:
         if not self.holding_state:
             return
         rx, ry, rz = r
-        rz = np.pi/2 + rz
+        # rz = np.pi/2 + rz
         T = transformation.euler_matrix(rx, ry, rz)
         pre_pos = np.array([x, y, z])
         pre_pos += self.pick_offset * T[:3, 2]
@@ -129,6 +129,6 @@ class UR5:
 if __name__ == '__main__':
     rospy.init_node('ur5')
     ur5 = UR5()
-    ur5.moveToP(-0.26, -0.1, 0.5, np.pi/4, 0, 0)
+    ur5.moveToP(-0.26, -0.1, 0.3, 0, np.pi/6, 0)
     # ur5.moveToP(-0.26, -0.1, 0.5, 0.3848295, 0.3588603, 0.7143543)
     # ur5.moveToJ(ur5.home_joint_values)