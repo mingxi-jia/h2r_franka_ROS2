3bdd5b2f7e49fbe5b177df8a8a46e8d74821c407
your_github_username <yixiang_sun@brown.edu>
2024-10-25 17:32:45 -0400

    refactor cloud_proxy_three

--- README.md
+++ README.md
@@ -4,11 +4,18 @@
 ### Instructions
 1. Cameras
     ```
-    roslaunch realsense2_camera rs_camera.launch serial_no:=234322306820 camera:=kevin align_depth:=true depth_width:=640 depth_height:=480 depth_fps:=30 color_width:=840 color_height:=480 color_fps:=30
-   roslaunch realsense2_camera rs_camera.launch serial_no:=234222301686 camera:=stuart align_depth:=true depth_width:=640 depth_height:=480 depth_fps:=30 color_width:=640 color_height:=480 color_fps:=30
-   roslaunch realsense2_camera rs_camera.launch serial_no:=234222300548 camera:=bob align_depth:=true depth_width:=640 depth_height:=480 depth_fps:=30 color_width:=640 color_height:=480 color_fps:=30
+    roslaunch realsense2_camera rs_camera.launch serial_no:=234322306820 camera:=bob align_depth:=true depth_width:=640 depth_height:=480 depth_fps:=15 color_width:=640 color_height:=480 color_fps:=15
+   roslaunch realsense2_camera rs_camera.launch serial_no:=f1420123 camera:=kevin align_depth:=true depth_width:=640 depth_height:=480 depth_fps:=15 color_width:=640 color_height:=480 color_fps:=15
+   roslaunch realsense2_camera rs_camera.launch serial_no:=234222301686 camera:=stuart align_depth:=true depth_width:=640 depth_height:=480 depth_fps:=15 color_width:=640 color_height:=480 color_fps:=15
+   roslaunch realsense2_camera rs_camera.launch serial_no:=239222303414 camera:=mel align_depth:=true depth_width:=640 depth_height:=480 depth_fps:=15 color_width:=640 color_height:=480 color_fps:=15
+   roslaunch realsense2_camera rs_camera.launch serial_no:=239222303046 camera:=dave align_depth:=true depth_width:=640 depth_height:=480 depth_fps:=15 color_width:=640 color_height:=480 color_fps:=15
+
     ```
 2. publish poses
-```
-roslaunch camera_pose_publisher.launch
-```
+    ```
+    roslaunch camera_pose_publisher.launch
+    ```
+3. open gripper with command line
+    ```
+    rostopic pub --once /franka_gripper/move/goal franka_gripper/MoveActionGoal "goal: { width: 0.1, speed: 0.1 }"
+    ```
--- src/cloud_proxy_three.py
+++ src/cloud_proxy_three.py
@@ -27,14 +27,18 @@ sys.path.append("/home/master_oogway/panda_ws/src/GEM2.0")
 
 class CloudProxy:
     def __init__(self):
-
-        self.cam_names = ['bob', 'kevin', 'stuart']
+        self.fixed_cam_names = ['dave', 'mel', 'kevin', 'bob', 'stuart']
+        # self.fixed_cam_names = ['stuart']
+        self.inhand_cam_name = 'tim'
+        self.cam_names = self.fixed_cam_names + [self.inhand_cam_name]
         self.base_frame = 'panda_link0'
 
         self.topic1_depth = "/bob/aligned_depth_to_color/image_raw"
         self.topic2_depth = "/kevin/aligned_depth_to_color/image_raw"
         self.topic3_depth = "/stuart/aligned_depth_to_color/image_raw"
         self.topic4_depth = "/tim/aligned_depth_to_color/image_raw"
+        self.topic5_depth = "/dave/aligned_depth_to_color/image_raw"
+        self.topic6_depth = "/mel/aligned_depth_to_color/image_raw"
         self.sub1_depth = rospy.Subscriber(self.topic1_depth, Image, self.call_back_depth1, queue_size=1)
         self.depth1 = None
         self.sub2_depth = rospy.Subscriber(self.topic2_depth, Image, self.call_back_depth2, queue_size=1)
@@ -43,11 +47,17 @@ class CloudProxy:
         self.depth3 = None
         self.sub4_depth = rospy.Subscriber(self.topic4_depth, Image, self.call_back_depth4, queue_size=1)
         self.depth4 = None
+        self.sub5_depth = rospy.Subscriber(self.topic5_depth, Image, self.call_back_depth5, queue_size=1)
+        self.depth5 = None
+        self.sub6_depth = rospy.Subscriber(self.topic6_depth, Image, self.call_back_depth6, queue_size=1)
+        self.depth6 = None
 
         self.topic1_info = "/bob/color/camera_info"
         self.topic2_info = "/kevin/color/camera_info"
         self.topic3_info = "/stuart/color/camera_info"
         self.topic4_info = "/tim/color/camera_info"
+        self.topic5_info = "/dave/color/camera_info"
+        self.topic6_info = "/mel/color/camera_info"
         self.sub1_info = rospy.Subscriber(self.topic1_info, CameraInfo, self.call_back_info1, queue_size=1)
         self.info1 = None
         self.sub2_info = rospy.Subscriber(self.topic2_info, CameraInfo, self.call_back_info2, queue_size=1)
@@ -56,11 +66,17 @@ class CloudProxy:
         self.info3 = None
         self.sub4_info = rospy.Subscriber(self.topic4_info, CameraInfo, self.call_back_info4, queue_size=1)
         self.info4 = None
+        self.sub5_info = rospy.Subscriber(self.topic5_info, CameraInfo, self.call_back_info5, queue_size=1)
+        self.info5 = None
+        self.sub6_info = rospy.Subscriber(self.topic6_info, CameraInfo, self.call_back_info6, queue_size=1)
+        self.info6 = None
 
         self.topic1_rgb = "/bob/color/image_raw"
         self.topic2_rgb = "/kevin/color/image_raw"
         self.topic3_rgb = "/stuart/color/image_raw"
         self.topic4_rgb = "/tim/color/image_raw"
+        self.topic5_rgb = "/dave/color/image_raw"
+        self.topic6_rgb = "/mel/color/image_raw"
         self.sub1_rgb = rospy.Subscriber(self.topic1_rgb, Image, self.call_back_rgb1, queue_size=1)
         self.rgbimg1 = None
         self.sub2_rgb = rospy.Subscriber(self.topic2_rgb, Image, self.call_back_rgb2, queue_size=1)
@@ -69,16 +85,25 @@ class CloudProxy:
         self.rgbimg3 = None
         self.sub4_rgb = rospy.Subscriber(self.topic4_rgb, Image, self.call_back_rgb4, queue_size=1)
         self.rgbimg4 = None
+        self.sub5_rgb = rospy.Subscriber(self.topic5_rgb, Image, self.call_back_rgb5, queue_size=1)
+        self.rgbimg5 = None
+        self.sub6_rgb = rospy.Subscriber(self.topic6_rgb, Image, self.call_back_rgb6, queue_size=1)
+        self.rgbimg6 = None
 
         self.rgb1_frame = "bob_color_optical_frame"
         self.rgb2_frame = "kevin_color_optical_frame"
         self.rgb3_frame = "stuart_color_optical_frame"
         self.rgb4_frame = "tim_color_optical_frame"
+        self.rgb5_frame = "dave_color_optical_frame"
+        self.rgb6_frame = "mel_color_optical_frame"
 
         # RT base_link
-        self.workspace = np.array([[0.28, 0.71],
-                                    [-0.32, 0.26],
-                                    [-0.1, 1.0]])
+        # self.workspace = np.array([[0.28, 0.71],
+        #                             [-0.32, 0.26],
+        #                             [-0.1, 1.0]])
+        self.workspace = np.array([[0.20, 0.75],
+                                    [-0.30, 0.25],
+                                    [-0.02, 1.0]])
         self.center = self.workspace.mean(-1)
         self.x_size = self.workspace[0].max() - self.workspace[0].min()
         self.x_half = self.x_size/2
@@ -154,6 +179,12 @@ class CloudProxy:
 
     def call_back_info4(self, msg):
         self.info4 = np.array(msg.K).reshape(3,3)
+    
+    def call_back_info5(self, msg):
+        self.info5 = np.array(msg.K).reshape(3,3)
+
+    def call_back_info6(self, msg):
+        self.info6 = np.array(msg.K).reshape(3,3)
 
     def call_back_depth1(self, msg):
         self.depth1 = ros_numpy.numpify(msg)
@@ -167,6 +198,12 @@ class CloudProxy:
     def call_back_depth4(self, msg):
         self.depth4 = ros_numpy.numpify(msg)
 
+    def call_back_depth5(self, msg):
+        self.depth5 = ros_numpy.numpify(msg)
+    
+    def call_back_depth6(self, msg):
+        self.depth6 = ros_numpy.numpify(msg)
+
     def call_back_rgb1(self, msg):
         self.rgbimg1 = ros_numpy.numpify(msg)
 
@@ -178,6 +215,12 @@ class CloudProxy:
     
     def call_back_rgb4(self, msg):
         self.rgbimg4 = ros_numpy.numpify(msg)
+    
+    def call_back_rgb5(self, msg):
+        self.rgbimg5 = ros_numpy.numpify(msg)
+
+    def call_back_rgb6(self, msg):
+        self.rgbimg6 = ros_numpy.numpify(msg)
 
     def get_depth_image(self, cam_id, iteration=10):
         images = []
@@ -186,18 +229,28 @@ class CloudProxy:
                 while self.depth1 is None:
                     rospy.sleep(0.01)
                 images.append(self.depth1/1000)
-            if cam_id == 2 or cam_id == 'kevin':
+            elif cam_id == 2 or cam_id == 'kevin':
                 while self.depth2 is None:
                     rospy.sleep(0.01)
                 images.append(self.depth2/1000)
-            if cam_id == 3 or cam_id == 'stuart':
+            elif cam_id == 3 or cam_id == 'stuart':
                 while self.depth3 is None:
                     rospy.sleep(0.01)
                 images.append(self.depth3/1000)
-            if cam_id == 4 or cam_id == 'tim':
+            elif cam_id == 4 or cam_id == 'tim':
                 while self.depth4 is None:
                     rospy.sleep(0.01)
                 images.append(self.depth4/1000)
+            elif cam_id == 5 or cam_id == 'dave':
+                while self.depth5 is None:
+                    rospy.sleep(0.01)
+                images.append(self.depth5/1000)
+            elif cam_id == 6 or cam_id == 'mel':
+                while self.depth6 is None:
+                    rospy.sleep(0.01)
+                images.append(self.depth6/1000)
+            else:
+                NotImplementedError
         image = np.median(images, axis=0)
         return image
 
@@ -206,44 +259,49 @@ class CloudProxy:
             while self.rgbimg1 is None:
                 rospy.sleep(0.01)
             return self.rgbimg1
-        if cam_id == 2 or cam_id == 'kevin':
+        elif cam_id == 2 or cam_id == 'kevin':
             while self.rgbimg2 is None:
                 rospy.sleep(0.01)
             return self.rgbimg2
-        if cam_id == 3 or cam_id == 'stuart':
+        elif cam_id == 3 or cam_id == 'stuart':
             while self.rgbimg3 is None:
                 rospy.sleep(0.01)
             return self.rgbimg3
-        if cam_id == 4 or cam_id == 'tim':
+        elif cam_id == 4 or cam_id == 'tim':
             while self.rgbimg4 is None:
                 rospy.sleep(0.01)
             return self.rgbimg4
+        elif cam_id == 5 or cam_id == 'dave':
+            while self.rgbimg5 is None:
+                rospy.sleep(0.01)
+            return self.rgbimg5
+        elif cam_id == 6 or cam_id == 'mel':
+            while self.rgbimg6 is None:
+                rospy.sleep(0.01)
+            return self.rgbimg6
+        else:
+            NotImplementedError
 
     
-    def transform_clip_cloud_to_base(self, cloud, cam_id):
+    def transform_cloud_to_base(self, cloud, cam_id):
         if cam_id == 1 or cam_id == 'bob':
             rgb_frame = self.rgb1_frame
-        if cam_id == 2 or cam_id == 'kevin':
+        elif cam_id == 2 or cam_id == 'kevin':
             rgb_frame = self.rgb2_frame
             self.proj_pose = self.lookup_transform(rgb_frame, self.base_frame, rospy.Time(0))
-        if cam_id == 3 or cam_id == 'stuart':
+        elif cam_id == 3 or cam_id == 'stuart':
             rgb_frame = self.rgb3_frame
+        elif cam_id == 4 or cam_id == 'tim':
+            rgb_frame = self.rgb4_frame
+        elif cam_id == 5 or cam_id == 'dave':
+            rgb_frame = self.rgb5_frame
+        elif cam_id == 6 or cam_id == 'mel':
+            rgb_frame = self.rgb6_frame
         # if cam_id == 4 or cam_id == 'tim':
         #     rgb_frame = self.rgb4_frame
         T = self.lookup_transform(rgb_frame, self.base_frame, rospy.Time(0))
         cloud_RT_base = self.transform(cloud[:, :3], T)
         return np.concatenate([cloud_RT_base, cloud[:, 3:]], axis=1)
-
-
-    def cloud_preprocess(self, cloud):
-        cloud, rgb_clip = self.getFilteredPointCloud(cloud[:, :3], cloud[:, 3:])
-        pcd = open3d.geometry.PointCloud()
-        pcd.points = open3d.utility.Vector3dVector(cloud)
-        cl, ind = pcd.remove_statistical_outlier(nb_neighbors=50, std_ratio=2.0)
-        cloud = np.asarray(cl.points)
-        rgb_clip = rgb_clip[ind]
-        cloud = np.concatenate([cloud, rgb_clip], axis=1)
-        return cloud
     
     
     def cloud_preprocess(self, cloud):
@@ -260,15 +318,21 @@ class CloudProxy:
         self.depth1 = None
         self.depth2 = None
         self.depth3 = None
+        self.depth4 = None
+        self.depth5 = None
+        self.depth6 = None
         self.rgbimg1 = None
         self.rgbimg2 = None
         self.rgbimg3 = None
+        self.rgbimg4 = None
+        self.rgbimg5 = None
+        self.rgbimg6 = None
         self.cloud1 = None
         self.cloud2 = None
         self.cloud3 = None
-        self.rgb1 = None
-        self.rgb2 = None
-        self.rgb3 = None
+        self.cloud4 = None
+        self.cloud5 = None
+        self.cloud6 = None
 
     def pad_bottom_cloud(self, cloud, rgb_value=[255,255,255]):
         # generate 'fake' point cloud for area outside the bins
@@ -307,6 +371,30 @@ class CloudProxy:
 
         return cloud, cloud1, cloud2, cloud3
     
+    def get_pointcloud_in_cam_frame(self, cam_name):
+        rgb = self.get_rgb_image(cam_name)
+        depth = self.get_depth_image(cam_name)
+        intrinsics = self.get_cam_intrinsic(cam_name)
+
+        height, width = depth.shape
+        xlin = np.linspace(0, width - 1, width)
+        ylin = np.linspace(0, height - 1, height)
+        px, py = np.meshgrid(xlin, ylin)
+        px = (px - intrinsics[0, 2]) * (depth / intrinsics[0, 0])
+        py = (py - intrinsics[1, 2]) * (depth / intrinsics[1, 1])
+        
+        points = np.float32([px, py, depth, rgb[..., 0], rgb[..., 1], rgb[..., 2]]).transpose(1, 2, 0)
+        cloud = points.reshape(-1,6)
+        # z_constrain= (cloud[:,2]>0.1) & (cloud[:,2]<1.1)
+        # cloud = cloud[z_constrain]
+        visualize = False
+        if visualize:
+            pcd = open3d.geometry.PointCloud()
+            pcd.points = open3d.utility.Vector3dVector(cloud[:,:3])
+            pcd.colors = open3d.utility.Vector3dVector(cloud[:, 3:6]/255)
+            open3d.visualization.draw_geometries([pcd])
+        return cloud
+
     def get_fused_cloud(self, instruction):
         self.clear_cache()
         cloud1 = self.get_pointcloud_from_depth(1)
@@ -421,6 +509,14 @@ class CloudProxy:
             while self.info4 is None:
                 rospy.sleep(0.01)
             intrinsics = self.info4
+        if cam_id == 5 or cam_id == 'dave':
+            while self.info5 is None:
+                rospy.sleep(0.01)
+            intrinsics = self.info5
+        if cam_id == 6 or cam_id == 'mel':
+            while self.info6 is None:
+                rospy.sleep(0.01)
+            intrinsics = self.info6
         return intrinsics
 
     def get_cam_extrinsic(self, cam_id):
@@ -436,6 +532,12 @@ class CloudProxy:
         if cam_id == 4 or cam_id == 'tim':
             rgb_frame = self.rgb4_frame
             extrinsic = self.lookup_transform(rgb_frame, self.base_frame, rospy.Time(0))
+        if cam_id == 5 or cam_id == 'dave':
+            rgb_frame = self.rgb5_frame
+            extrinsic = self.lookup_transform(rgb_frame, self.base_frame, rospy.Time(0))
+        if cam_id == 5 or cam_id == 'mel':
+            rgb_frame = self.rgb6_frame
+            extrinsic = self.lookup_transform(rgb_frame, self.base_frame, rospy.Time(0))
         return extrinsic
     
     def get_all_cam_extrinsic(self):
@@ -449,7 +551,42 @@ class CloudProxy:
         for cam_id in self.cam_names:
             intrinsics[cam_id] = self.get_cam_intrinsic(cam_id)
         return intrinsics
-        
+    
+    def get_whole_pc_in_robot_frame(self, allow_in_hand=False):
+        clouds = []
+        for cam in self.fixed_cam_names:
+            cloud = self.get_pointcloud_in_cam_frame(cam)
+            cloud = self.transform_cloud_to_base(cloud, cam)
+            clouds.append(cloud)
+
+        if allow_in_hand:
+            cloud_inhand = self.get_pointcloud_in_cam_frame(self.inhand_cam_name)
+            cloud_inhand = self.transform_cloud_to_base(cloud_inhand, 4)
+            clouds.append(cloud_inhand)
+
+        cloud = np.concatenate(clouds, axis=0)
+        return cloud
+    
+    def get_workspace_pc(self, allow_in_hand=False):
+        self.clear_cache()
+        cloud = self.get_whole_pc_in_robot_frame(allow_in_hand)
+        # filter ws x
+        x_cond = (cloud[:, 0] < self.center[0] + self.x_half) * (cloud[:, 0] > self.center[0] - self.x_half)
+        cloud = cloud[x_cond]
+        # filter ws y
+        y_cond = (cloud[:, 1] < self.center[1] + self.y_half) * (cloud[:, 1] > self.center[1] - self.y_half)
+        cloud = cloud[y_cond]
+        # filter ws z
+        z_cond = (cloud[:, 2] < self.center[2].max()) * (cloud[:, 2] > self.z_min)
+        cloud = cloud[z_cond]
+
+        pcd = open3d.geometry.PointCloud()
+        pcd.points = open3d.utility.Vector3dVector(cloud[:,:3])
+        cl, ind = pcd.remove_statistical_outlier(nb_neighbors=50, std_ratio=3.5)
+        cloud = cloud[ind]
+
+        return cloud
+
 
 def main():
     rospy.init_node('test')
@@ -470,8 +607,27 @@ def main():
     #     rgbc = cloud_proxy._preProcessRGBC(rgbc)
     cloud_proxy.get_pointcloud_from_depth(1, 'cube')
 
+def test_point_cloud():
+    rospy.init_node('test')
+    cloud_proxy = CloudProxy()
+    cloud = cloud_proxy.get_workspace_pc()
+    pcd = open3d.geometry.PointCloud()
+    pcd.points = open3d.utility.Vector3dVector(cloud[:,:3])
+    pcd.colors = open3d.utility.Vector3dVector(cloud[:, 3:6]/255)
+    open3d.visualization.draw_geometries([pcd])
 
+    # For saving pcds and images
+
+    # for cam_name in cloud_proxy.fixed_cam_names:
+    #     image = cloud_proxy.get_rgb_image(cam_name)
+    #     np.save('/home/master_oogway/panda_ws/code/helping_hands_panda/data/skye_teapot_data/rgb_{}_3.npy'.format(cam_name), image)
+
+    # np.save('/home/master_oogway/panda_ws/code/helping_hands_panda/data/skye_teapot_data/pcd_points_3.npy', np.array(pcd.points))
+    # np.save('/home/master_oogway/panda_ws/code/helping_hands_panda/data/skye_teapot_data/pcd_colors_3.npy', np.array(pcd.colors))
+
+    print(1)
 
 
 if __name__ == '__main__':
-    main()
+    # main()
+    test_point_cloud()
